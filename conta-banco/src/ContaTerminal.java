257023384941f410749e841c6cfd0c0650470738
Vverdum <vverdum@gmail.com>
2024-03-28 20:29:42 -0300

    Atualiza conta-terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,30 +1,36 @@
+import java.text.NumberFormat;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args)  {
-        
+    
        
-            Scanner sanner = new Scanner(System.in).useLocale(Locale.US);
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.println("Por favor, digite seu nome: ");
         String nomeCliente = scanner.next();
 
         System.out.println("Por favor, digite sua agência: ");
-        int agencia = scanner.next();
+        int agencia = scanner.nextInt();
 
         System.out.println("Por favor, digite o número de sua conta: ");
-        int numero = scanner.next();
+        int numero = scanner.nextInt();
 
         System.out.println("Por favor, digite seu saldo: ");
-        double saldo = scanner.next();
+        double saldo = scanner.nextDouble();
+
+        Locale brasil = new Locale("pt", "BR");
+        NumberFormat formatadorMoeda = NumberFormat.getCurrencyInstance(brasil);
+
+        String valorFormatado = formatadorMoeda.format(saldo);
         
-        System.out.println("Olá " + nomeCliente+", obrigado por criar uma conta em nosso banco,");
-        System.out.println("sua agência é " + agencia+"conta "+numero);
-        System.out.println("e seu saldo "+saldo+" já está disponível para saque");
-         
 
-
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco,");
+        System.out.println("sua agência é " + agencia + ", e o número de sua conta é " + numero);
+        System.out.println("e seu saldo " + valorFormatado + " já está disponível para saque.");
+        
+        scanner.close();
+    }
         
     }
-}
