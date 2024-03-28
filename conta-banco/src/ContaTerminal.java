d5e965c8031cc1f322a04d846bd2879a572b7965
Vverdum <vverdum@gmail.com>
2024-03-28 19:58:15 -0300

    atualizar Conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+        
+       
+            Scanner sanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite seu nome: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite sua agência: ");
+        int agencia = scanner.next();
+
+        System.out.println("Por favor, digite o número de sua conta: ");
+        int numero = scanner.next();
+
+        System.out.println("Por favor, digite seu saldo: ");
+        double saldo = scanner.next();
+        
+        System.out.println("Olá " + nomeCliente+", obrigado por criar uma conta em nosso banco,");
+        System.out.println("sua agência é " + agencia+"conta "+numero);
+        System.out.println("e seu saldo "+saldo+" já está disponível para saque");
+         
+
+
+        
     }
 }
